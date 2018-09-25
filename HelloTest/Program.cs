ecc1b02d58d7b72f1d0a313ef82c4b5bc9b2dd57
Brittany Heller <brittany.heller@live.com>
2018-09-24 20:07:45 -0600

    Added number picker

--- HelloTest/Program.cs
+++ HelloTest/Program.cs
@@ -6,7 +6,38 @@ namespace HelloTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random randomNumber = new Random();
+            int randNum = randomNumber.Next(1, 100);
+            Console.WriteLine("Please pick a number between 1 and 100");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your choice was " + userChoice);
+            numberPicker(userChoice, randNum);
+
+        }
+
+        public static void numberPicker(int a, int rand)
+        {
+            if (a == rand)
+            {
+                Console.WriteLine("You picked it! The number was " + rand);
+                Console.ReadLine();
+            }
+            else
+            {
+                if (a < rand)
+                {
+                    Console.WriteLine("Incorrect, guess higher!");
+                    int userChoice = Convert.ToInt32(Console.ReadLine());
+                    numberPicker(userChoice, rand);
+                }
+                else if (a > rand)
+                {
+                    Console.WriteLine("Incorrect, guess lower!");
+                    int userChoice = Convert.ToInt32(Console.ReadLine());
+                    numberPicker(userChoice, rand);
+                }
+            }
+            
         }
     }
 }
